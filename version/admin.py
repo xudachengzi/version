2ce9cda983fed66de40881fa807370a5608f0a85
SC-201812191507\Administrator <xu649047654@163.com>
2019-01-12 10:05:59 +0800

    add_all_files

---
+++ version/admin.py
@@ -0,0 +1,48 @@
+from django.contrib import admin
+
+from PaperlessVersion.settings import BASE_DIR
+from .models import PaperlessVersion
+import hashlib
+import qrcode
+import time
+import image
+
+
+# admin.site.register(PaperlessVersion)
+
+
+@admin.register(PaperlessVersion)
+class PaperlessVersionAdmin(admin.ModelAdmin):
+    list_display = ('name',
+                    'type',
+                    'version',
+                    'md5',
+                    'qr_code',
+                    'test_report',
+                    'content',
+                    'datetime_modified',
+                    )
+
+    # list_per_page设置每页显示多少条记录，默认是100条
+    list_per_page = 50
+
+    # ordering设置默认排序字段，负号表示降序排序
+    ordering = ('-type',
+                '-datetime_modified',)
+
+    # list_editable = [
+    #                  'version']
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        with open(obj.file_path.path, 'rb') as fp:
+            f_md5 = hashlib.md5(fp.read()).hexdigest()
+            obj.md5 = f_md5
+        create_time = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+        path1 = '192.168.0.118:8000' + '/files/' + str(obj.file_path)
+        path2 = 'files/img/' + create_time + '.png'
+        img = qrcode.make(path1)
+        with open(path2, 'wb') as fp:
+            img.save(fp)
+        obj.qr_code = '/img/' + create_time + '.png'
+        obj.save()
